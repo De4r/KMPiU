0002091765ac1db8034dadcece3789fa39e9e419
De4r <mkrupnik96@gmail.com>
2019-04-09 12:05:28 +0200

    09.04

--- Lab5/szlaban_new_An.m
+++ Lab5/szlaban_new_An.m
@@ -2,7 +2,7 @@ clc
 clear all
 
 t0=0;
-tk=5;
+tk=3;
 n=2000;
 l=0.5;
 tr = linspace(t0,tk,n);
@@ -10,38 +10,44 @@ tr = linspace(t0,tk,n);
 pocz = 0;
 if pocz==1
     y0=[0,0];
+    nazwa1='podnoszeniefi.png';
+    nazwa2='podnoszeniey.png'
 elseif pocz==0
     y0 = [0, pi/2];
+    nazwa1='opuszczaniefi.png';
+    nazwa2='opuszczaniey.png';
 end
 
 [t,y] = ode45(@(t,x) szlaban_new(t,x,pocz), tr, y0);
-yy = y*2*l;
+yy = y*4*l;
 
 y = rad2deg(y);
 figure(10)
 subplot(211)
 plot(t,y(:,1))
 xlabel('Czas[s]')
-title('Predkosc kat. dla \phi, War. 1')
-ylabel('Predkosc katowa [\circ/s]')
+title('Prêdkoœæ k¹towe dla \phi')
+ylabel('Prêdkoœæ k¹towa [\circ/s]')
 grid on
 subplot(212)
 plot(t,y(:,2))
 xlabel('Czas[s]')
-title('Przemieszczenie kat. dla \phi, War. 1')
-ylabel('Przemieszczenie katowe [\circ]')
+title('Przemieszczenie k¹towe dla \phi')
+ylabel('Przemieszczenie k¹towe [\circ]')
 grid on
+saveas(gcf, nazwa1)
 
 figure(11)
 subplot(211)
 plot(t,yy(:,1))
 xlabel('Czas[s]')
-title('Predkosc dla y=\phi*2l, War. 1')
-ylabel('Predkosc [m/s]')
+title('Prêdkoœæ dla y=\phi*4l')
+ylabel('Prêdkoœæ [m/s]')
 grid on
 subplot(212)
 plot(t,yy(:,2))
 xlabel('Czas[s]')
-title('Przemieszczenie dla y=\phi*2l, War. 1')
+title('Przemieszczenie dla y=\phi*4l')
 ylabel('Przemieszczenie [m]')
 grid on
+saveas(gcf, nazwa2)
