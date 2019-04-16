8cdfaef785eff6a569e0c4318b51a01e21594990
De4r <mkrupnik96@gmail.com>
2019-04-16 15:34:54 +0200

    16.04

--- Lab5/szlaban_new.m
+++ Lab5/szlaban_new.m
@@ -3,15 +3,9 @@ function dQ = szlaban_new(t, Q, pocz)
 dfi=Q(1);
 fi=Q(2);
 
-k=2000;
-m1=6;
-m2=1;
-m3 =3;
-b=7000;
-g=9.81;
-e=0.15; l=2/4;
-
+k=2000; m1=6; m2=1; m3 =3; b=7000; g=9.81; e=0.15; l=2/4;
 Me = 100;
+
 if pocz==1
     if fi>= 0 && fi< pi/2
         Me = Me;
@@ -25,31 +19,15 @@ elseif pocz==0
         Me = Me;
     end
 end
-% if pocz==1
-%     if fi>= 0 && fi< pi/3
-%         Me = Me*0.8;
-%     elseif fi >= pi/3 && fi < pi/2
-%         Me = Me*fi*(1-fi/pi/2);
-%     elseif fi > pi/2
-%         Me = -Me*0.2;
-%     end
-% elseif pocz==0
-%     if fi <= pi/2 && fi > pi/3
-%         Me = -Me*fi/pi/2;
-%     elseif fi <= pi/3 && fi > 0
-%         Me = -Me*0.3;
-%     elseif fi < 0
-%         Me = Me;
-%     end
-% end
-%     
-
     
 J = [(16*m1*l^2+m2*e^2+m3*e^2*(sin(pi/4-fi))^2)/3, 1];
 M = diag(J);
 
-W = [-k*e^2*sin(pi/4-fi)*(cos(pi/4-fi)-cos(pi/4)) - b*e^2*dfi*(sin(pi/4 - fi))^2 + Me + (m2/2+m3)*g*e*cos(pi/4-fi) - m1*g*2*l*cos(fi) + 2*m3*e^2*(dfi^2)*sin(pi/4-fi)*cos(pi/4-fi);
-    dfi];
+F = -k*e^2*sin(pi/4-fi)*(cos(pi/4-fi)-cos(pi/4)) -...
+    b*e^2*dfi*(sin(pi/4 - fi))^2 + Me + (m2/2+m3)*g*e*cos(pi/4-fi) -...
+    m1*g*2*l*cos(fi) + 2*m3*e^2*(dfi^2)*sin(pi/4-fi)*cos(pi/4-fi);
 
+W = [F;
+    dfi];
 
 dQ = M \ W;
